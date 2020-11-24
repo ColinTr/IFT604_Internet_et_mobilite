285fac6b5986a46bab36e601ca606e94115eb378
GuillaumeBchd <guillaumebouchard@orange.fr>
2020-11-24 13:54:54 +0100

    added security catch

--- serveur_express/src/controllers/kognotte.controller.js
+++ serveur_express/src/controllers/kognotte.controller.js
@@ -40,42 +40,46 @@ exports.createTransaction = async (req, res) => {
         req.body.object === undefined ||
         req.body.date === undefined) {
 
-        logger.error(
-            "Contenu manquant dans le body de la requête d'ajout de kourse."
-        );
+        logger.error("Contenu manquant dans le body de la requête d'ajout de kourse.");
         return res.status(400).send(new Erreur("Contenu manquant dans le body de la requête d'ajout de transaction."))
     }
     else {
-        
-        const transaction = await transactionService.addTransaction(config.MONGODB_DASHBOARD_ID, 
-            req.body.from, 
-            req.body.to, 
-            req.body.montant,
-            req.body.object,
-            new Date(req.body.date));
-        
-        if (transaction === undefined) {
-            return res.status(502).send(new Erreur("Le serveur n'a pas réussi à créer la transaction"));
-        }
 
-        // We update the solde from the user 'from'
-        const change = req.body.montant;
-        const resFrom = await soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, req.body.from, {$inc : {value: change }}); 
-        // We await here in case we change again the value in the users 'to' 
-        
-        // We update all the sold from the users 'to'
-        const mapPromiseSoldeUpdate = req.body.to.map( (element) => {
-            const change =  -(req.body.montant / req.body.to.length);
-            return soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, element, {$inc : {value: change}})
-        });
-        
-        // We wait for all the promises
-        const resTo = await Promise.all(mapPromiseSoldeUpdate);
-        if (resFrom === undefined || resTo.includes(undefined)){
+        try {
+            const transaction = await transactionService.addTransaction(config.MONGODB_DASHBOARD_ID, 
+                req.body.from, 
+                req.body.to, 
+                req.body.montant,
+                req.body.object,
+                new Date(req.body.date));
+            
+            if (transaction === undefined) {
+                return res.status(502).send(new Erreur("Le serveur n'a pas réussi à créer la transaction"));
+            }
+    
+            // We update the solde from the user 'from'
+            const change = req.body.montant;
+            const resFrom = await soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, req.body.from, {$inc : {value: change }}); 
+            // We await here in case we change again the value in the users 'to' 
+            
+            // We update all the sold from the users 'to'
+            const mapPromiseSoldeUpdate = req.body.to.map( (element) => {
+                const change =  -(req.body.montant / req.body.to.length);
+                return soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, element, {$inc : {value: change}})
+            });
+            
+            // We wait for all the promises
+            const resTo = await Promise.all(mapPromiseSoldeUpdate);
+            if (resFrom === undefined || resTo.includes(undefined)){
+                return res.status(502).send(new Erreur("Something went wrong during the process of your request"));
+            }
+    
+            return res.send();
+        }
+        catch(error){
+            log.error(error);
             return res.status(502).send(new Erreur("Something went wrong during the process of your request"));
-        }
-
-        return res.send();
+        }    
     }
 };
 
@@ -85,31 +89,40 @@ exports.deleteTransaction = async (req, res) => {
         return res.status(400).send(new Erreur("Contenu manquant dans le body de la requête d'ajout de transaction."))
     }
     else {
-        const transaction = await transactionService.getTransaction(req.body.id);
-        const trIsDeleted = await transactionService.removeTransaction(req.body.id);
-        console.log(transaction);
 
-        if (trIsDeleted === undefined) {
-            return res.status(502).send(new Erreur("Le serveur n'a pas réussi à supprimer la transaction"));
+        try{
+
+            const transaction = await transactionService.getTransaction(req.body.id);
+            const trIsDeleted = await transactionService.removeTransaction(req.body.id);
+            console.log(transaction);
+
+            if (trIsDeleted === undefined) {
+                return res.status(502).send(new Erreur("Le serveur n'a pas réussi à supprimer la transaction"));
+            }
+
+            // We update the solde from the user 'from'
+            const change = - transaction.montant;
+            const resFrom = await soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, transaction.from, {$inc : {value: change }}); 
+            // We await here in case we change again the value in the users 'to' 
+            
+            // We update all the sold from the users 'to'
+            const mapPromiseSoldeUpdate = transaction.to.map( (element) => {
+                const change =  (transaction.montant / transaction.to.length);
+                return soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, element, {$inc : {value: change}})
+            });
+
+            // We wait for all the promises
+            const resTo = await Promise.all(mapPromiseSoldeUpdate);
+            if (resFrom === undefined || resTo.includes(undefined)){
+                return res.status(502).send(new Erreur("Something went wrong during the process of your request"));
+            }
+
+            return res.send();
+
         }
-
-        // We update the solde from the user 'from'
-        const change = - transaction.montant;
-        const resFrom = await soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, transaction.from, {$inc : {value: change }}); 
-        // We await here in case we change again the value in the users 'to' 
-        
-        // We update all the sold from the users 'to'
-        const mapPromiseSoldeUpdate = transaction.to.map( (element) => {
-            const change =  (transaction.montant / transaction.to.length);
-            return soldeService.updateSoldeFromDashboardAndUser(config.MONGODB_DASHBOARD_ID, element, {$inc : {value: change}})
-        });
-
-        // We wait for all the promises
-        const resTo = await Promise.all(mapPromiseSoldeUpdate);
-        if (resFrom === undefined || resTo.includes(undefined)){
+        catch(error){
+            logger.error(error);
             return res.status(502).send(new Erreur("Something went wrong during the process of your request"));
         }
-
-        return res.send();
     }
 };
